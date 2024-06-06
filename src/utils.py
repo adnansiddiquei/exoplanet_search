08daafa388ac58b9cf6b2017e438afab5f6717f7
adnansiddiquei <adnanshirik@gmail.com>
2024-06-06 22:25:16 +0100

    Initial implementation with transitlightcurves package.

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ from scipy.signal import savgol_filter, medfilt
 import numpy as np
 import pickle
 import os
+import matplotlib.pyplot as plt
 
 
 def normalise(data, method, window_length, **kwargs):
@@ -10,16 +11,17 @@ def normalise(data, method, window_length, **kwargs):
 
     if method == 'savgol':
         filter = savgol_filter(
-            data, window_length=window_length, polyorder=kwargs['polyorder']
+            data['flux'], window_length=window_length, polyorder=kwargs['polyorder']
         )
     elif method == 'median':
-        filter = medfilt(data, kernel_size=window_length)
+        filter = medfilt(data['flux'], kernel_size=window_length)
     else:
         raise ValueError(f'Normalization method {method} not recognized')
 
-    normalized_data = data / filter
+    data['flux'] = data['flux'] / filter
+    data['flux_error'] = data['flux_error'] / filter
 
-    return normalized_data
+    return data
 
 
 def identify_outliers(data, threshold=3.0):
@@ -76,3 +78,57 @@ def create_dir_if_required(script_filepath: str, dir_name: str) -> str:
         os.makedirs(dir_to_make)
 
     return dir_to_make
+
+
+def plot_folded_lightcurve(tls_results, plot_model=True):
+    fig, ax = plt.subplots()
+
+    plt.scatter(
+        (tls_results.folded_phase * tls_results.period) - (tls_results.period / 2),
+        tls_results.folded_y,
+        color='blue',
+        s=1,
+        alpha=0.5,
+        zorder=2,
+    )
+
+    if plot_model:
+        plt.plot(
+            (tls_results.model_folded_phase * tls_results.period)
+            - (tls_results.period / 2),
+            tls_results.model_folded_model,
+            color='red',
+        )
+
+    plt.xlabel('Phase')
+    plt.ylabel('Relative flux')
+
+    return fig, ax
+
+
+def calc_transit_duration(tls_results):
+    tmp = (
+        tls_results.model_folded_phase[tls_results.model_folded_model < 1]
+        * tls_results.period
+    )
+    return tmp[-1] - tmp[0]
+
+
+def plot_periodogram(tls_results):
+    fig, ax = plt.subplots(figsize=(10, 5))
+
+    periods = tls_results.periods  # Array of tested periods
+    power = tls_results.power  # Corresponding power for each period
+    mask = periods < (tls_results.period * 4.2)
+
+    # Plot the periodogram
+    plt.plot(periods[mask], power[mask], 'k-')
+    plt.xlabel('Period (days)')
+    plt.ylabel('Power')
+
+    plt.axvline(tls_results.period, color='red', linestyle='dashed', linewidth=1)
+    plt.axvline(tls_results.period * 2, color='red', linestyle='dashed', linewidth=1)
+    plt.axvline(tls_results.period * 3, color='red', linestyle='dashed', linewidth=1)
+    plt.axvline(tls_results.period * 4, color='red', linestyle='dashed', linewidth=1)
+
+    return fig, ax
